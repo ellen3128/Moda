3ba0508d99eb132a8b9dc106739871f7b02514c7
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-24 02:58:18 -0400

    add a button on the homepage

--- front-end/src/views/HomePage.vue
+++ front-end/src/views/HomePage.vue
@@ -1,12 +1,15 @@
 <template>
-    <v-row>
+    <v-row class="grid">
         <v-col cols="2">
-        <v-card-text>
-          Lorem ipsum...
+        <v-card-text class="text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium adipisci qui amet voluptatibus libero maxime distinctio aliquam, in quo corporis, est cumque eveniet debitis voluptates vitae. Expedita soluta distinctio quod.
         </v-card-text>
       </v-col>
       <v-col cols="10">
+        <div style="position: relative;">
         <v-img :src="coverImage" cover height="100%"></v-img>
+        <v-btn class="centered-btn" style="z-index: 1;">My Button</v-btn>
+        </div>
       </v-col>
     </v-row>
   </template>
@@ -24,11 +27,20 @@
   
 
 <style scoped>
-h1 {
-    color: lightblue;
+.grid {
+    margin-top: 2rem;
+}
+.text {
+    font-size: 12px;
 }
 
 .color {
     background-color: blue;
+}
+.centered-btn {
+    position: absolute;
+    top: 80%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>

029bd95861055745957c708b3316205239bf9cdc
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-23 23:28:28 -0400

    sets up initial view pages (home, product, details, cart) with routes

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,23 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import CartPage from '../views/CartPage'
+import ProductDetailPage from '../views/ProductDetailPage'
+import ProductsPage from '../views/ProductsPage'
+import HomePage from '../views/HomePage'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+{
+  path: '/products',
+  name: 'Products',
+  component: ProductsPage,
+}, {
+  path: '/products/:id',
+  name: 'ProductDetail',
+  component: ProductDetailPage,
+}, {
+  path: '/cart',
+  name: 'Cart',
+  component: CartPage,
+}, {
+  path: '/',
+  name: 'Home',
+  component: HomePage,
+}
 ]
 
 const router = new VueRouter({
---
+++ front-end/src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1> This is the Homepage </h1>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+}
+</script>
+
+<style scoped>
+h1 {
+    color: purple;
+}
+</style>

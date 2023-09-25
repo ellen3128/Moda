68de785856b1e627e86f798ff9eb2094e688b8bb
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-25 11:25:48 -0400

    updates cart page with new products list and products list item compoments

--- front-end/src/views/CartPage.vue
+++ front-end/src/views/CartPage.vue
@@ -1,18 +1,7 @@
 <template>
   <div id="page-wrap">
-    <h1>Shopping Cart</h1>
-    <div
-      v-for="product in cartItems"
-      :key="product.id"
-      class="product-container"
-    >
-      <img class="product-image" :src="product.imageUrl" />
-      <div class="details-wrap">
-        <h3>{{ product.name }}</h3>
-        <p>${{ product.price }}</p>
-      </div>
-      <button class="remove-button">Remove From Cart</button>
-    </div>
+    <h1> Shopping Cart </h1>
+    <ProductsList :products="cartItems"/>
     <h3 id="total-price">Total: ${{ totalPrice }}</h3>
     <button id="checkout-button">Proceed to Checkout</button>
   </div>
@@ -20,9 +9,13 @@
 
 <script>
 import { cartItems } from '../fake-data';
+import ProductsList from '../components/ProductsList.vue';
 
 export default {
     name: 'CartPage',
+    components: {
+      ProductsList,
+    },
     data() {
       return {
         cartItems,

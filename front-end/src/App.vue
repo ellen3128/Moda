da66ce1dade8cd79d97d8b10a25bb7c5f9e18e72
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-28 00:21:57 -0400

    adds footer

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <NavBar />
     <router-view/>
+    <FooterSection />
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
+import FooterSection from './components/FooterSection.vue';
 export default {
   name: 'App',
   components: {
     NavBar,
+    FooterSection,
   }
 }
 </script>
---
+++ front-end/src/components/FooterSection.vue
@@ -0,0 +1,58 @@
+<template>
+    <footer class="footer-container">
+      <h6> copyright all rights reserved © MODA.inc </h6>
+      <!-- Footer Links -->
+      <div class="footer-links">
+        <!-- <router-link to="/about">About Us</router-link> -->
+        <router-link class="link" to="/terms">Terms & Conditions</router-link>
+        <router-link class="link" to="/privacy">Privacy Policy</router-link>
+      </div>
+  
+    
+  
+      <!-- Newsletter Subscription -->
+      <!-- <div class="newsletter-form">
+        <input v-model="email" placeholder="Subscribe to our newsletter" />
+        <button @click="subscribe">Subscribe</button>
+      </div> -->
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: ''
+      };
+    },
+    // methods: {
+    //   subscribe() {
+    //     // Logic for handling newsletter subscription
+    //     console.log(`Subscribed with: ${this.email}`);
+    //   }
+    // }
+  };
+  </script>
+  
+  <style scoped>
+  .footer-container {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    background-color: #3c3c3c;
+    color: #fff;
+    margin-top: 5rem;
+  }
+  
+  .footer-links {
+    display: flex;
+    gap: 50px;
+    color: white;
+  }
+
+  .link, h6 {
+    color: white;
+  }
+  
+  </style>
+  

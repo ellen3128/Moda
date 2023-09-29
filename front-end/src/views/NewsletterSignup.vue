a89c52e6350f19b62e029956a0fee2209307f712
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-29 00:54:13 -0400

    adds styling to subscription page

--- front-end/src/views/NewsletterSignup.vue
+++ front-end/src/views/NewsletterSignup.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <div class="form">
       <h2>Subscribe to Our Newsletter</h2>
       <form @submit.prevent="subscribe">
         <div class="form-group">
@@ -18,6 +19,7 @@
       </form>
       <p v-if="subscribed">Thank you for subscribing!</p>
       <p v-if="error">{{ error }}</p>
+    </div>
     </div>
   </template>
   
@@ -54,13 +56,59 @@
     },
   };
   </script>
-  
-  <style scoped>
 
-  .form-group {
-    margin: 0 auto;
+<style scoped>
+.form {
+  max-width: 400px;
+  margin: 15rem auto;
+  padding: 2rem;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.05);
+}
 
-  }
+h2 {
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
 
-  </style>
-  
+.form-group {
+  margin-bottom: 1.5rem;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type="email"] {
+  width: 100%;
+  padding: 0.75rem;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+  font-size: 1rem;
+}
+
+button[type="submit"] {
+  background-color: #007BFF;
+  color: #ffffff;
+  padding: 0.75rem 1.5rem;
+  font-size: 1rem;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button[type="submit"]:hover {
+  background-color: #0056b3;
+}
+
+p {
+  color: red;
+}
+
+p[v-if="subscribed"] {
+  color: green;
+}
+</style>
